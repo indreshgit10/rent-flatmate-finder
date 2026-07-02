440c88a0e310e3311ffb18a6121dabc57338d03c
indreshgit10 <indreshkumar456789@gmail.com>
2026-07-02 22:48:49 +0530

    feat: socket chat events

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,6 @@
 const { Server } = require('socket.io');
+const ChatMessage = require('./models/ChatMessage');
+const InterestRequest = require('./models/InterestRequest');
 
 let io;
 
@@ -12,6 +14,35 @@ const initSocket = (server) => {
 
   io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
+
+    socket.on('join_chat', (interestId) => {
+      socket.join(interestId);
+      console.log(`Socket ${socket.id} joined chat ${interestId}`);
+    });
+
+    socket.on('send_message', async (data) => {
+      try {
+        const { interestId, senderId, content } = data;
+        
+        const interest = await InterestRequest.findById(interestId);
+        if (!interest || interest.status !== 'accepted') {
+          return socket.emit('error', 'Cannot send message to this chat');
+        }
+
+        const msg = await ChatMessage.create({
+          interestRequest: interestId,
+          sender: senderId,
+          content
+        });
+
+        const populatedMsg = await msg.populate('sender', 'name role');
+
+        io.to(interestId).emit('receive_message', populatedMsg);
+      } catch (err) {
+        console.error('Socket send_message error:', err);
+        socket.emit('error', 'Failed to send message');
+      }
+    });
 
     socket.on('disconnect', () => {
       console.log('User disconnected:', socket.id);
